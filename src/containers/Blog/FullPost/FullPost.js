e9f256c628d962714a2de27816f85cff0ee174b5
Santosh <santoshkumarm1996@gmail.com>
2020-04-03 18:36:19 +0530

    Added React Routing (03/0/2019)

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -13,7 +13,7 @@ class FullPost extends Component {
     componentDidMount() {
         console.log(this.props)
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)) {
                 axios.get("/posts/" + this.props.match.params.id).then((response) => {
                     this.setState({ loadedPost: response.data })
                 })
@@ -23,13 +23,13 @@ class FullPost extends Component {
     }
 
     deletePostHandler=()=>{
-        axios.delete("/posts/" + this.props.id).then((res)=>{
+        axios.delete("/posts/" + this.props.match.params.id).then((res)=>{
             console.log("res",res)
         })
     }
     render() {
         let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
-        if (this.props.id !== null) {
+        if (this.props.match.params.id !== null) {
             post = <p style={{ textAlign: "center" }}>Loading...!</p>;
         }
         if (this.state.loadedPost !== null) {
